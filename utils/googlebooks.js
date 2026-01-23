1704d618175f46cac1c49ca0186f5fd1af2beb93
sabir-yas <yaseersabir005@gmail.com>
2026-01-22 20:22:34 -0700

    first push- changes made to googlebooks json, headers text file and readme

--- utils/googlebooks.js
+++ utils/googlebooks.js
@@ -1,24 +1,20 @@
 const axios = require('axios')
 
 module.exports = async (phrase) => {
-    const results = await axios.get('https://www.googleapis.com/books/v1/volumes', {
+    const response = await axios.get('https://www.googleapis.com/books/v1/volumes', {
         params: {
             format: 'json',
             q: `${phrase}`
         }
     })
 
-
-    //
-    //TODO: return a formattedObj
     var formattedObj = {
-        data: results.data,
-        status: results.status,
-        statusText: results.statusText,
-        headers: results.headers,
-        requestHeader: results.config.headers
+        data: response.data,
+        status: response.status,
+        statusText: response.statusText,
+        headers: response.headers,
+        requestHeader: response.config.headers
     }
 
-    //TODO: return the formattedObject
-    return JSON.stringify(results.data);
+    return formattedObj
 }
